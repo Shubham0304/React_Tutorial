84bf17cd1e46d859c70fcb2dff9b025918b10a8f
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-05-30 03:34:11 +0530

    React basics: lists and arrays

--- Part-1/social-media-website/src/App.js
+++ Part-1/social-media-website/src/App.js
@@ -1,58 +1,29 @@
 import './App.css';
+import {User} from "./User"
 
 
 
 function App() {
-  // const name = "Shubham";
-  // const age = <h2> 31 </h2>;
-  // const email = <h2>shubhamjoshi0304@gmail.com</h2>
-  // const user = (
-  //   <div>
-  //     {name}
-  //     {age}
-  //     {email}
-  //   </div>
-  // );
-  return (
-    <div className="App"> Hey Man!! These are my details: 
-    {/* {user} */}
-    <User name="Shubham" age={31} email="shubhamjoshi000304@gmail.com"/>
-    <User name="Jake" age={36} email="jakegym@gmail.com"/>
-    <User name="Reemi" age={41} email="remi@gmail.com"/>
-    <Job salary={30000} position="SDE" company="Amazon"/>
-    <Job salary={50000} position="Manager" company="FCL"/>
-    <Job salary={89999} position="CEO" company="MFT"/>
-    </div>
-  );
-}
+// const names = ["Pedro", "Jake", "Jessica", "Mike", "Dustin"];
+const users = [{name: "Pedro", age:21}, {name:"Jake", age: 25}, {name: "Jessica", age:45}];
 
-// const getName = () => {
-//   return "Shubham"
-// }
+return ( <div className="App">
+    {/* {
+        names.map((name,key) => {
+            return <h1> {name}</h1>;
+        })
+    } */}
 
-// const GetNameComponent = () => {
-//   return <h1> Shubham </h1>
-// }
+    {
+        users.map((user,key)=>{
+            return (
+                <User name= {user.name} age= {user.age}></User>
+            )
+        })
+    }
+</div>
+);
 
-const User = (props) => {
- return (
-    <div style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.email}</h1>
-    </div>
- )
-
-}
-
-const Job = (props)=> {
-  return (
-    <div style={{ border: "1px solid blue", margin: "10px", padding: "10px" }}>
-      <h2>{props.salary}</h2>
-      <h2>{props.position}</h2>
-      <h2>{props.company}</h2>
-    </div>
-  )
 
 }
 
