5ef0875ab97620dc53e66849534a8e3a0e3871f3
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-06-02 05:28:39 +0530

    Promises in js basics

--- Part-1/social-media-website/src/js_basics.js
+++ Part-1/social-media-website/src/js_basics.js
@@ -88,6 +88,19 @@ event.then((name)=>{
 console.log(name);
 }).catch((error)=> {
     console.log(error);
-}).finally(()=> {
+}).finally(()=> { 
     console.log("Promise resolved!!")
+})
+
+
+const axios = require("axios");
+
+const data =axios.get("https://cat-fact.herokuapp.com/facts");
+//console.log(data);
+data.then((res) => {
+console.log(res.data);
+}).catch((err)=> {
+    console.log("Error: ",err)
+}).finally(()=> {
+    console.log("Promise resolved");
 })
