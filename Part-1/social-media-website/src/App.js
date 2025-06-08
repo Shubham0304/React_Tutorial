cd6925c0625059546fd362ccc37dd8a28a8c61ba
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-06-08 05:49:36 +0530

    State hooks examples

--- Part-1/social-media-website/src/App.js
+++ Part-1/social-media-website/src/App.js
@@ -4,14 +4,48 @@ import { useState } from 'react';
 
 
 function App() {
-    const [age, setAge] = useState(0)
-    const increaseAge = ()=> {
-        setAge(age + 1)
-    };
 
-    return <div className="App"> {age} 
-    <button onClick={increaseAge}>Increase Age</button></div>
+    //StateHooks example1 : 
+    // const [age, setAge] = useState(0)
+    // const increaseAge = ()=> {
+    //     setAge(age + 1)
+    // };
 
+     //StateHooks example2 : 
+    // const [inputValue, setInputValue] = useState("");
+
+    // const handleInputChange = (event) => {
+    //     //console.log(event.target.value);
+    //     setInputValue(event.target.value);
+    // }
+
+    //StateHooks Example 3: 
+    const [showText,setShowText] = useState(true);
+
+    const handleButtonChange = (event) => {
+        setShowText(!showText);
+    }
+
+    // example1 : 
+    return (
+    // <div className="App"> {age} 
+    // <button onClick={increaseAge}>Increase Age</button>
+    // </div>
+        //StateHooks example2 : 
+
+    // Example2: 
+    // <div className='App'>
+    //     <input type='text' onChange={handleInputChange}></input>
+    //     {inputValue}
+    // </div>
+
+    // Example 3: 
+        <div className='App'>
+            <button onClick={handleButtonChange}> Show/Hide</button>
+            {showText ? <h1>Hi My Name is Shubham</h1> : null}
+        </div>
+
+    );
 
 }
 
