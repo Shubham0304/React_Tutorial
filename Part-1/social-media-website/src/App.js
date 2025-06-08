6c3014eac20257da02c3cb2be338bd2ca12136b3
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-06-09 03:28:07 +0530

    state hooks example and a basic to do list app

--- Part-1/social-media-website/src/App.js
+++ Part-1/social-media-website/src/App.js
@@ -5,45 +5,46 @@ import { useState } from 'react';
 
 function App() {
 
-    //StateHooks example1 : 
-    // const [age, setAge] = useState(0)
-    // const increaseAge = ()=> {
-    //     setAge(age + 1)
-    // };
+    const [todoList, setTodoList] =useState([]);
+    const [newTask, setNewTask] = useState("");
 
-     //StateHooks example2 : 
-    // const [inputValue, setInputValue] = useState("");
-
-    // const handleInputChange = (event) => {
-    //     //console.log(event.target.value);
-    //     setInputValue(event.target.value);
-    // }
-
-    //StateHooks Example 3: 
-    const [showText,setShowText] = useState(true);
-
-    const handleButtonChange = (event) => {
-        setShowText(!showText);
-    }
-
-    // example1 : 
+    const addTask = () => {
+        const newToDoList = [...todoList, newTask];
+        setTodoList(newToDoList);
+    } 
     return (
-    // <div className="App"> {age} 
-    // <button onClick={increaseAge}>Increase Age</button>
-    // </div>
-        //StateHooks example2 : 
-
-    // Example2: 
-    // <div className='App'>
-    //     <input type='text' onChange={handleInputChange}></input>
-    //     {inputValue}
-    // </div>
-
-    // Example 3: 
         <div className='App'>
-            <button onClick={handleButtonChange}> Show/Hide</button>
-            {showText ? <h1>Hi My Name is Shubham</h1> : null}
+            <div className='addTask'>
+                <input  onChange={ (event)=> {
+                    setNewTask(event.target.value);
+                }}/>
+                <button onClick={addTask}>Add Task</button>
+            </div>
+            <div className='list'>
+                {todoList.map( (task,index)=>{
+                    return  <div key={index}>
+                        <h1>{task}</h1>
+                        <button onClick = {()=> {
+    //Approach 1: 
+                            // const updatedTaskList = todoList.filter((taskName)=> {
+                            //     if (task===taskName) {
+                            //         return false;
+                            //     }
+                            //     else {
+                            //         return true;
+                            //     }
+                            // } )
+    // Approach 2:  
+                                const updatedTaskList = todoList.filter((taskName)=> {
+                                    return task !==taskName
+                                })
+                            setTodoList(updatedTaskList);
+                        }}>Delete Task</button>
+                         </div>
+                } )}
+            </div>
         </div>
+        
 
     );
 
