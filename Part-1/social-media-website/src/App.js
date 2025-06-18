c9383bb494ffbc4295b9e2fae9b3a14dc43adfff
Shubham0304 <shubhamjoshi0304@gmail.com>
2025-06-18 06:12:46 +0530

    state changes

--- Part-1/social-media-website/src/App.js
+++ Part-1/social-media-website/src/App.js
@@ -4,19 +4,46 @@ import { Home } from './pages/Home';
 import { Menu } from './pages/Menu';
 import { Contact } from './pages/Contact';
 import { Navbar } from './Navbar';
+import { useState } from 'react';
 
 
 
 function App() {
 
+// Example of Props drilling
+    // const TopComponent = ()=> {
+    //     const [state, setState] = useState();
+    //     return (
+    //         <div >
+    //             <MiddleComponent state={state} />
+    //         </div>
+    //     )
+    // }
+    //     const MiddleComponent = ()=> {
+    //     // const [state, setState] = useState();
+    //     return (
+    //         <div >
+    //             <BottomComponent state={state} />
+    //         </div>
+    //     )
+    // }
+    //     const BottomComponentComponent = (state)=> {
+    //     // const [state, setState] = useState();
+    //     return (
+    //         <div >
+    //             {state}
+    //         </div>
+    //     )
+    // }
+    const [userName, setUserName] = useState("Shubham");
 
     return (
         <div className='App'>
             <Router>
                 <Navbar/>
                 <Routes>
-                    <Route path="/" element= {<Home/>}/>
-                    <Route path="/menu" element= {<Menu/>}/>
+                    <Route path="/" element= {<Home username={userName}/>}/>
+                    <Route path="/profile" element= {<Profile username={userName}/>}/>
                     <Route path="/contact" element= {<Contact/>}/>
                     <Route path="*" element= {<h1>Page Not Found</h1>}/>
 
