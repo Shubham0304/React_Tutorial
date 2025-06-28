3db0d6ca93ea01b26f61dc9087c8a887f4965789
shjo <shjo@stibodx.com>
2025-06-28 20:09:18 +0200

    Deleted social-media-website files

--- Part-1/social-media-website/src/Planet.js
+++
@@ -1,8 +0,0 @@
-export const Planet = (props) => {
-    return (
-        <div>
-           <h1> {props.name} </h1>
-        </div>
-    )
-}
-
--- Part-1/social-media-website/src/pages/Contact.js
+++
@@ -1,3 +0,0 @@
-export const Contact = () => {
-    return <h1> This is the Contact Page</h1>
-}  
