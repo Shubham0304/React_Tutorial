9b009ea5a0cac672c629392c8e18572d3f5b0c80
shjo <shjo@stibodx.com>
2025-07-02 00:55:34 +0200

    firebase project

---
+++ part-2/firebase_project/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div >
+      <Router>
+        <Routes>
+          <Route>
+            
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
