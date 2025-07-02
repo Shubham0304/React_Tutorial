405bf0f85fca0aa2c0f625728502c2b7a621b64d
shjo <shjo@stibodx.com>
2025-07-03 00:54:57 +0200

    Setting up basic routes and components for firebase project

--- part-2/firebase_project/src/App.tsx
+++ part-2/firebase_project/src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Main } from './pages/main';
+import { Login } from './pages/login';
+import { Navbar } from './components/navbar';
+
 
 
 function App() {
   return (
     <div >
       <Router>
+        <Navbar/>
         <Routes>
-          <Route>
-            
-          </Route>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </Router>
     </div>
---
+++ part-2/firebase_project/src/pages/main.tsx
@@ -0,0 +1,6 @@
+export const Main = () => {
+
+    return (
+        <h1>Home Page</h1>
+    ) 
+}
