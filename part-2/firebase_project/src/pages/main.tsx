ad6e89be70a80c342d5d25a930d876c1622b510d
shjo <shjo@stibodx.com>
2025-07-14 00:58:54 +0200

    adding logic to add data to the db

--- part-2/firebase_project/src/pages/main.tsx
+++ part-2/firebase_project/src/pages/main.tsx
@@ -1,4 +1,35 @@
+import {getDocs, collection} from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useEffect, useState } from "react";
+
+
+interface Post {
+    id: string,
+    userId: string,
+    username: string,
+    title: string,
+    description: string
+}
+
 export const Main = () => {
+
+    const [postsList, setPostsList] = useState<Post[]|null>(null);
+    const postsRef = collection(db, "posts") 
+
+
+    const getPosts = async () => {
+
+        const data = await getDocs(postsRef);
+        setPostsList(data.docs.map(
+            (doc)=> ({...doc.data(), id: doc.id})) as Post[]);
+    }
+
+    useEffect( () => {
+        
+    })
+
+
+    getPosts();
 
     return (
         <h1>Home Page</h1>
